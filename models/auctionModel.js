79f919fc52c5f7aba8d76881ca2f630cfe7e8ba2
Swagata kumar Kundu <swagatakr.kundu@gmail.com>
2017-10-04 16:01:15 +0530

    auction model commit

--- models/auctionModel.js
+++ models/auctionModel.js
@@ -10,8 +10,11 @@ auction.uploadVehicle = function (req, callback) {
         basic_info,
         specification,
         features,
-        inspection_report
+        inspection_report,
+        images,
     } = req.body;
+
+    var omitKeys = ['images', 'max_power', 'max_torque'];
 
     const {
         dimensions_weight,
@@ -20,7 +23,21 @@ auction.uploadVehicle = function (req, callback) {
         suspension_breaks_steering_tyres
     } = specification;
 
+    const {
+        max_power,
+        max_torque
+    } = engine_taransmission;
+
     insertObject = lodash.assign({}, basic_info, dimensions_weight,
         capacity, engine_taransmission, suspension_breaks_steering_tyres);
+
+    insertObject = lodash.omit(insertObject, omitKeys);
+
+    insertObject.max_power_bhp = max_power.bhp;
+    insertObject.max_power_rpm = max_power.rpm;
+
+    insertObject.max_torque_nm = max_torque.nm;
+    insertObject.max_torque_rpm = max_torque.rpm;
+    
     return callback(null, insertObject);
 };
