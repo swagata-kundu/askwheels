ac38d60c909c485b6b34492db48cf772d0c7b74c
swagata-kundu <swagatakr.kundu@gmail.com>
2017-11-23 17:58:49 +0530

    wishlist end point changes

--- router/auctionRouter.js
+++ router/auctionRouter.js
@@ -7,10 +7,18 @@ router.get('/vehicle/:vehicleId', controllerIndex.auction.auctionDetail);
 router.post('/vehicle', controllerIndex.auction.uploadVehicle);
 router.post('/seller', controllerIndex.auction.auctionListSeller);
 router.post('/dealer', controllerIndex.auction.auctionListDealer);
-router.post('/vehicle/admin/list', authUtils.verifyAdmin, controllerIndex.auction.vehicleListAdmin);
-router.put('/vehicle/admin/status', authUtils.verifyAdmin, controllerIndex.auction.changeVehicleStatus);
+router.post(
+    '/vehicle/admin/list',
+    authUtils.verifyAdmin,
+    controllerIndex.auction.vehicleListAdmin
+);
+router.put(
+    '/vehicle/admin/status',
+    authUtils.verifyAdmin,
+    controllerIndex.auction.changeVehicleStatus
+);
 router.get('/features', controllerIndex.auction.listFeatures);
-router.put('/wishlist', controllerIndex.wishlist.addDealerWishList);
-router.get('/wishlist', controllerIndex.wishlist.dealerWishList);
+router.put('/dealer/wishlist', controllerIndex.wishlist.addDealerWishList);
+router.get('/dealer/wishlist', controllerIndex.wishlist.dealerWishList);
 
 module.exports = router;
